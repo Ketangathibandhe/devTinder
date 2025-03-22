208eab13073effa057da627e8bb29a2083d626ba
Ketan Gathibandhe <ketangathibandhe04@gmail.com>
2025-03-22 17:00:40 +0530

    exploring GET , POST,DEL http methods

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
-console.log("new project")
+const express = require('express');
+
+const app = express();
+
+app.use("/test",(req,res)=>{
+    res.send("Hello from '/'")
+});
+app.use("/one",(req,res)=>{
+    res.send("Hello from '/one'")
+});
+app.use("/two",(req,res)=>{
+    res.send("Hello from '/two'")
+});
+
+app.listen(3001,()=>{
+    console.log("liestning to port 3000...")
+})
