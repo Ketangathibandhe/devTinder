b157db0b9739348c51d6c8d1498b289446f8b740
Ketan Gathibandhe <ketangathibandhe04@gmail.com>
2025-06-02 17:15:40 +0530

    playing with route

--- src/app.js
+++ src/app.js
@@ -1,16 +1,45 @@
 const express = require("express");
 
 const app = express();
-app.use("/",(req,res)=>{
-    res.send("///////")
-})
-app.use("/hello",(req,res)=>{
-    res.send("Hello Hello Hello")
+// app.use("/",(req,res)=>{
+//     res.send("///////")
+// })
+// app.use("/hello",(req,res)=>{
+//     res.send("Hello Hello Hello")
+// })
+
+// app.use((req,res)=>{
+//     res.send("Hello from the server")
+// })
+
+// app.use("/page1",(req,res)=>{
+//     res.send("Your are at the page one")
+// })
+// app.use("/page2",(req,res)=>{
+//     res.send("Your are at the page two")
+// })
+// app.use("/page3",(req,res)=>{
+//     res.send("Your are at the page three")
+// })
+
+//if we use app.use it will match all the api calls
+
+//rather doind app.use we can use app.get to handel only GET http call
+app.get("/page1",(req,res)=>{
+    res.send({firsename:"Ketan",lastname:"Gathibandhe"})   //this will handle only GET request 
 })
 
-app.use((req,res)=>{
-    res.send("Hello from the server")
+app.post("/user",(req,res)=>{
+    console.log("Sucessfully saved the data")
+    res.send("The data has been saved to the database")
+})
+
+app.delete("/delete",(req,res)=>{
+    res.send("Data deleted")
 })
 app.listen(3000,()=>{
     console.log("Server is listening on port 3000")
 })
+
+//http methods (POST,PATCH,GET,DELETE)
+//to test our API we use POSTMAN 
