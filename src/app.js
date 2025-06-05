5812029e0570305f4d0013d358b42dbf1d72cf2f
Ketan Gathibandhe <ketangathibandhe04@gmail.com>
2025-06-05 12:38:56 +0530

    multiple route handler

--- src/app.js
+++ src/app.js
@@ -25,35 +25,50 @@ const app = express();
 //if we use app.use it will match all the api calls
 
 //rather doind app.use we can use app.get to handel only GET http call
-app.get(/ab+cd/, (req, res) => {
-  res.send({ firsename: "Ketan", lastname: "Gathibandhe" }); //this will handle only GET request
-});
+// app.get(/ab+cd/, (req, res) => {
+//   res.send({ firsename: "Ketan", lastname: "Gathibandhe" }); //this will handle only GET request
+// });
 
-//now if we want to add regex in the path the we should write the path in /..../
+// //now if we want to add regex in the path the we should write the path in /..../
 
-app.post("/user", (req, res) => {
-  console.log("Sucessfully saved the data");
-  res.send("The data has been saved to the database");
-});
+// app.post("/user", (req, res) => {
+//   console.log("Sucessfully saved the data");
+//   res.send("The data has been saved to the database");
+// });
 
-app.delete("/delete", (req, res) => {
-  res.send("Data deleted");
-});
+// app.delete("/delete", (req, res) => {
+//   res.send("Data deleted");
+// });
 
-app.get("/data", (req, res) => {
-  console.log(req.query); //localhost:3000/data?userid=10&password=1234 will print an {userid:10,password:1234} because it contains query parameter
-  res.send("The data has been received");
-});
+// app.get("/data", (req, res) => {
+//   console.log(req.query); //localhost:3000/data?userid=10&password=1234 will print an {userid:10,password:1234} because it contains query parameter
+//   res.send("The data has been received");
+// });
 
-//dyanamic routes
-app.get("/data/:userid/:password", (req, res) => {
-  console.log(req.params);
-  res.send("The data is collected ");
-});
+// //dyanamic routes
+// app.get("/data/:userid/:password", (req, res) => {
+//   console.log(req.params);
+//   res.send("The data is collected ");
+// });
 
-app.listen(3000, () => {
-  console.log("Server is listening on port 3000");
-});
+// app.listen(3000, () => {
+//   console.log("Server is listening on port 3000");
+// });
 
 //http methods (POST,PATCH,GET,DELETE)
 //to test our API we use POSTMAN
+
+// multiple route handler
+
+app.use(
+  "/user",
+  (req, res, next) => {
+    res.send("Response!!!");
+    next();
+  },
+  (req, res) => {
+    res.send("Response 2!!");
+  }
+);
+
+app.listen(3000);
